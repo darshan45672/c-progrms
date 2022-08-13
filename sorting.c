47c9ee9e5322ebc101393022140b5fda219bbf47
darshan45672 <109609462+darshan45672@users.noreply.github.com>
2022-08-13 19:42:02 +0530

    Add files via upload

---
+++ sorting.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[50], n, i, j, temp;
+  printf("enter the number of elements\n");
+  scanf("%d", &n);
+  printf("enter %d elements\n",n);
+  for (i = 0; i < n; i++) {
+    printf("%\t", a[i]);
+  }
+  for (i = 1; i < n; i++) {
+    for (j = 0; j < n - i; i++) {
+      if (a[j] > a[j + 1]) {
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
+    }
+  }
+  printf("the sorted elements are\n");
+  for (i = 0; i < n; i++) {
+    printf("%d\t", a[i]);
+  }
+  printf("\n");
+}
