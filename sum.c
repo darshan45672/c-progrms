28bd9ac56cd88d68f3008762a6430b73ed403f78
darshan45672 <109609462+darshan45672@users.noreply.github.com>
2022-08-13 19:39:07 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+  //using 3 variables
+  
+  /* int a,b;
+  printf("enter the value of a=\n");
+  scanf("%d",&a);
+   printf("enter the value of b=\n");
+  scanf("%d",&b);
+  int sum=a+b;
+  printf("sum = %d",sum); */
+
+  //using 2 variable 
+
+  int a,b;
+  
+  printf("enter the value of a=\n");
+  scanf("%d",&a);
+   printf("enter the value of b=\n");
+  scanf("%d",&b);
+  printf("sum is %d",a+b);
+  return 0;
+}
