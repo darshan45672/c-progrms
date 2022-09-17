b63c0969bfb6724aa43b981ba7a56372108dae2d
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-09-17 19:39:59 +0530

    Add files via upload

---
+++ copying string using pointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  char str1[100],str2[100];
+  char *ptr1=str1;
+  char *ptr2=str2;
+  printf("enter string 1:\n");
+  scanf("%s",str1);
+  while(*ptr1!='\0'){
+    *ptr2=*ptr1;
+    ptr1++;
+    ptr2++;
+  }
+  *ptr2='\0';
+  printf("copied string is %s\n",str2);
+}
