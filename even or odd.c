a9c54997187c6ead60613a652bb8e40f1e321a1f
darshan45672 <drshnbhandary@gmail.com>
2022-11-06 09:00:11 +0530

    new commit

---
+++ even or odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+  printf("enter an integer:\n");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("%d is an even number!\n",n);
+  }
+  else
+   {
+      printf("%d is an odd number!\n",n);
+   }
+}
