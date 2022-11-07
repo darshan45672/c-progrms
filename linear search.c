de0b14f9ef2eedd1fe4a757fc3e5380cfb8eb9ab
darshan45672 <drshnbhandary@gmail.com>
2022-11-07 07:47:40 +0530

    new source file

---
+++ linear search.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[100], i, n, key;
+  printf("enter numbers of elemnts:");
+  scanf("%d", &n);
+  printf("\nenter %d elements\n", n);
+  for (i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  printf("enter the element to be searched:\n");
+  scanf("%d", &key);
+  for (i = 0; i < n; i++) {
+    if (a[i] == key) {
+      printf("search sucessful!\n",
+             key, a[i]);
+      exit(0);
+    }
+  }
+  printf("search unsucessful!\n element %d is not found in the given list!\n",
+         key);
+}
