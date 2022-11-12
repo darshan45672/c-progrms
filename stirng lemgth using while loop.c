12633624a9fd7feecaa8f3629d36eb5cce822732
darshan45672 <drshnbhandary@gmail.com>
2022-11-12 07:25:52 +0530

    new source file

---
+++ stirng lemgth using while loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  char str[100];
+  int i;
+  printf("enter the string:\t");
+  scanf("%s",str);
+  i=0;
+  while(str[i]!='\0'){
+    i++;
+  }
+  printf("length of the string is %d",i);
+}
