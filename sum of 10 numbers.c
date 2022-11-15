53a5722ad8282601642378093045e8a8892271c2
darshan45672 <drshnbhandary@gmail.com>
2022-11-15 18:40:17 +0530

    new source file

---
+++ sum of 10 numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,m,sum=0;
+  printf("\n enter the value of m:");
+  scanf("%d",&n);
+  while(m<=n)
+    {
+      sum=sum+m;
+      m=m+1;
+    }
+  printf("\n sum= %d",sum);
+}
