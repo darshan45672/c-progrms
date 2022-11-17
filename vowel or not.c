b9549618ff96356641b0a8dc77bd582d4d393f11
darshan45672 <drshnbhandary@gmail.com>
2022-11-17 07:56:05 +0530

    new comment

--- vowel or not.c
+++ vowel or not.c
@@ -4,6 +4,7 @@ int main(void) {
   char ch;
   printf("enter any alphabet:\n");
   scanf("%c", &ch);
+  // condition to check vowels
   if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" ||
       ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U") {
     printf("\n%c is a vowel!", ch);
