df9d647d4eeb1a42bbdfa90f8487869649784594
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-12-07 20:13:31 +0530

    Add files via upload

---
+++ multiplication_table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("krupaya apka mann pasanda number likiye:");
+  scanf("%d",&num);
+  for(int i=1;i<=10;i++)
+    {
+      printf("%d X %d = %d\n",num,i,num*i);
+    }
+  return 0;char
+}
