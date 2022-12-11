e3456574aeee0a5b814105b6a17feb29e60b43e2
darshan45672 <drshnbhandary@gmail.com>
2022-12-11 13:26:02 +0530

    new

--- sorting.c
+++ sorting.c
@@ -1,4 +1,4 @@
-/* Program  to implement sorting on integers, This program sorts the  */
+/* Program  to implement sorting on integers, This program sorts the integers taken by the  */
 
 #include <stdio.h>
 
