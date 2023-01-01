545114a9b6bf7898afff2c79c60e677231005965
darshan45672 <drshnbhandary@gmail.com>
2023-01-01 18:58:24 +0530

    new comment

--- +ve or -ve.c
+++ +ve or -ve.c
@@ -6,7 +6,7 @@ int main(void) {
   int num;
   printf("enter a number:\n");    // taking input from the user
   scanf("%d",&num);
-  if(num==0){                   //checking wether the number entered by  the user is equal to 
+  if(num==0){                   //checking wether the number entered by  the user is equal to 0 or not
     printf("\nthe value is equal to zero");
     }else if(num>0){
     printf("\nthe number is positive");
