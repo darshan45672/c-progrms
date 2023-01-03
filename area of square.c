4f10fd22a617fc7b937138cc016dafeaea28b1d6
darshan45672 <drshnbhandary@gmail.com>
2023-01-03 12:24:50 +0530

    updated

--- area of square.c
+++ area of square.c
@@ -8,7 +8,7 @@ int main(void) {
   //area of a square
  int side;
   printf("eneter the value of side of a square =");  //taking the user input
-  scanf("%d",&side);
-  printf("the area of a square is %d",side*side);
+  scanf("%d",&side); 
+  printf("the area of a square is %d",side*side);    // displaying the calculated area using 
   return 0;
   }
