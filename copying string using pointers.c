7fea827fb328950dea15e252cd1d705d0fa1519a
darshan45672 <drshnbhandary@gmail.com>
2023-01-12 07:53:57 +0530

    file updated

--- copying string using pointers.c
+++ copying string using pointers.c
@@ -7,11 +7,11 @@ int main(void) {
   char str1[100],str2[100];  // declaring 2 char arrays 
   char *ptr1=str1;      // decalring and intialsing 2 pointers
   char *ptr2=str2;
-  
+
   printf("enter string 1:\n");  // taking user input
   scanf("%s",str1); // storing the string in str1
 
-  while(*ptr1!='\0'){
+  while(*ptr1!='\0'){   // looping the ptr variable till it approaches terminating character of the str1
     *ptr2=*ptr1;
     ptr1++;
     ptr2++;
