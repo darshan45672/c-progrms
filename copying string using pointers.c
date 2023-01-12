788b856a703faea4ea12f45d60d8a0cd8c24ca38
darshan45672 <drshnbhandary@gmail.com>
2023-01-12 07:51:38 +0530

    file updated

--- copying string using pointers.c
+++ copying string using pointers.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 int main(void) {
-  char str1[100],str2[100];
-  char *ptr1=str1;
+  char str1[100],str2[100];  // declaring 2 char arrays 
+  char *ptr1=str1;      // decalring and intialsing 2 pointers
   char *ptr2=str2;
   printf("enter string 1:\n");
   scanf("%s",str1);
