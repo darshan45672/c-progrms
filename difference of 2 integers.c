7f6f0a912d7e677403a82158ec51df458ce28e57
darshan45672 <drshnbhandary@gmail.com>
2023-01-15 16:01:22 +0530

    file updated

--- difference of 2 integers.c
+++ difference of 2 integers.c
@@ -8,6 +8,10 @@ int main(void) {
 
   printf("enter the values of a and b:\n");  // asking the user to enter the values
   scanf("%d%d",&a,&b); // storing the entered values and store it in variables a and b
-  res=a-b;
-  printf("difference of two numbers is %d",res);
+
+  res=a-b;  // performing subraction operation on a and b, and store it in a variable res
+
+  printf("difference of two numbers is %d",res); // display the value in res
+
+ return 0; //  end of main
 }
