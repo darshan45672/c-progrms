c4f65be61c9c8b481be1629ec4ebc2cd9d98f9e7
darshan45672 <drshnbhandary@gmail.com>
2023-01-15 15:59:49 +0530

    file updated

--- difference of 2 integers.c
+++ difference of 2 integers.c
@@ -7,7 +7,7 @@ int main(void) {
  int a,b,res;  // declaring variables
 
   printf("enter the values of a and b:\n");  // asking the user to enter the values
-  scanf("%d%d",&a,&b); // storing the entered values
+  scanf("%d%d",&a,&b); // storing the entered values and store it in variables a and b
   res=a-b;
   printf("difference of two numbers is %d",res);
 }
