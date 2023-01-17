de8f12e079aac8b59a147f8e892f0983d607bea1
darshan45672 <drshnbhandary@gmail.com>
2023-01-17 08:01:55 +0530

    updated code

--- display exam result.c
+++ display exam result.c
@@ -6,9 +6,11 @@ int main(void) {
 
 
   int marks; // declaring required variables
-  printf("\n enter the marks obtianed:\n");
-  scanf("%d",&marks);
-  if(marks>=75){
+
+  printf("\n enter the marks obtianed:\n"); // taking input from the user
+  scanf("%d",&marks); // storing the entered data in a variable in a variable named marks
+
+  if(marks>=75){              // checking wether the marks is greater than 75 using if-else ladder
     printf("\n DISTINCTION");
     }
   else if(marks>=60&&marks<75){
