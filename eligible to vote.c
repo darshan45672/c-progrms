7ac3931abfd421e62c21f8839a2d98d77c8c4a8e
darshan45672 <drshnbhandary@gmail.com>
2023-01-20 00:27:36 +0530

    updated

--- eligible to vote.c
+++ eligible to vote.c
@@ -5,15 +5,14 @@
 int main(void) {
 
  int age;  // ddeclaring the required variable
- 
+
   printf("enter the age:\n"); // taking input from the user
-  scanf("%d",&age);
-  if(age>=18)
-  {
+  scanf("%d",&age); // storing the user entered data in a variable
+
+  if(age>=18){  //checkong wether age is greater than or equal to 18  if true display the relavent message
     printf("you are eligible to vote!\n");
   }
-  else
-  {
+  else{   // if the above condition is false display the relavent message
     printf("SORRY!\nyou are not eligible to vote!");
   }
 }
