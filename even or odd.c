dcdb6723e3abd8577b2822e6f146d77af69fcf89
darshan45672 <drshnbhandary@gmail.com>
2023-01-21 21:20:10 +0530

    done

--- even or odd.c
+++ even or odd.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 
 int main(void) {
-  int n;
-  printf("enter an integer:\n");
-  scanf("%d",&n);
-  if(n%2==0)
-  {
+
+  int n; // declaring the required variables
+
+  printf("enter an integer:\n");  // taking input from the user
+  scanf("%d",&n);  // storing the user input in a variable
+  if(n%2==0) {  // checking wether the entered number gives 0 remainder when it is divided with 2
     printf("%d is an even number!\n",n);
   }
-  else
-   {
+  else{
       printf("%d is an odd number!\n",n);
    }
+  return 0; // end of main
 }
