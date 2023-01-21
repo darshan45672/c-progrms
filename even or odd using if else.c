804e0d6b0b6ff1168cc58c69b17caf282a53f5b0
darshan45672 <drshnbhandary@gmail.com>
2023-01-21 17:34:23 +0530

    formatted

--- even or odd using if else.c
+++ even or odd using if else.c
@@ -7,9 +7,10 @@ int main(void) {
   int num,rem; // declaring the rquired variables
 
   printf("\n enter the number(1-50):\n"); // taking input from the user
-  scanf("%d",&num);
-  rem=num%2;
-  switch(rem)
+  scanf("%d",&num);  // storing the user entered data in variable num
+
+  rem=num%2;  // performing modulo operation with 2
+  switch(rem) 
     {
       case 0: printf("\n even!");
       break;
