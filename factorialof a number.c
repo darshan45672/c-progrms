0cbdce08f67b986ba861260aa636bd827289db68
darshan45672 <drshnbhandary@gmail.com>
2023-01-23 06:18:17 +0530

    fromatted code

--- factorialof a number.c
+++ factorialof a number.c
@@ -7,14 +7,21 @@ int main(void )
 {
 int n,res; // declaring the required variables
   printf("enter a number\n");  // taling input from the user
-  scanf("%d",&n);
-res=fact(n);
-printf("factorial =%d",res); 
+  scanf("%d",&n); // storing the data in a variable
+
+  res = fact(n);  // caling the function fact() and storing the returned value in a variable res
+
+  printf("factorial =%d",res);  // display the value of res
+
+ return 0;  // end of main
+
 }
-int fact(int n)
-{
-if(n==0)
-return 1; 
+
+/* function defination of fact(n) */
+
+int fact(int n){
+  if(n==0)
+    return 1; 
 else
 return (n*fact(n-1));
   }
