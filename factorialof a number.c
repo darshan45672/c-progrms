9c5b2cb7a2859d1db79712b359ea227540d8d7a6
darshan45672 <drshnbhandary@gmail.com>
2023-01-23 06:20:52 +0530

    fromatted code

--- factorialof a number.c
+++ factorialof a number.c
@@ -23,5 +23,5 @@ int fact(int n){
   if(n==0)
     return 1; // defining the base / terminating condition
   else
-    return (n*fact(n-1));
+    return (n*fact(n-1));  // calling the function fact() passing the parametre as n-1 
   }
