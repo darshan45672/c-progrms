9f7475906abbeed0e6384bbecbeba4a593f3c249
darshan45672 <drshnbhandary@gmail.com>
2023-01-27 19:02:33 +0530

    new commit

--- integers between n and m.c
+++ integers between n and m.c
@@ -5,11 +5,13 @@
 int main(void) {
 
   int m, n, i;  // declaring the required  varaibles
-  
-  printf("enter the value of m:\n");
+
+  /* Taking the user input and storing it in a variable */
+  printf("enter the value of m:\n"); 
   scanf("%d", &m);
   printf("enter the value of n:\n");
   scanf("%d", &n);
+
   for (i = m; i <= n; i++) {
     printf("\t%d", i);
   }
