a712a69d7f2376246f161d12afc2969f04a7fd04
darshan45672 <drshnbhandary@gmail.com>
2023-01-29 17:23:04 +0530

    new code

--- largest of 3 nums using && oprt.c
+++ largest of 3 nums using && oprt.c
@@ -3,17 +3,23 @@
 #include <stdio.h>
 
 int main(void) {
-  int num1,num2,num3;
+
+  int num1,num2,num3;   // declaring the reqiured variables
+
+  /* taking input from the user */
   printf("\nenter the first number:");
-  scanf("%d",&num1);
+  scanf("%d",&num1);  // storing the data in a variable 
   printf("\n enter the second number:");
   scanf("%d",&num2);
   printf("\n enter the third number:");
   scanf("%d",&num3);
+
+  /* */
+  
   if(num1>num2&&num1>num3){
     printf("\n %d is the largest number!",num1);
     }
-    else if(num2>num1&&num2>num3){
+  else if(num2>num1&&num2>num3){
     printf("\n %d is the largest number!");
     }
   else{
