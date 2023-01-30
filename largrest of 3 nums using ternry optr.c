29e3da4ec93f08d9d2605b5cf07223ca047f6cf1
darshan45672 <drshnbhandary@gmail.com>
2023-01-30 07:56:34 +0530

    new update

--- largrest of 3 nums using ternry optr.c
+++ largrest of 3 nums using ternry optr.c
@@ -8,7 +8,8 @@ int main(void) {
  int a,b,c,big;  // declaring the required variables
 
   printf("enter 3 numbers:\n");  // taking 3 numbers from the  user
-  scanf("%d%d%d",&a,&b,&c);
+  scanf("%d%d%d",&a,&b,&c);  // storing the user entered data in appropriate  variables
+  
   big=a>b?(a>c?a:c):(b>c?b:c);
   printf("%d is the largest number",big);
 }
