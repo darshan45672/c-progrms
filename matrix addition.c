9268369e71222ab8e6595614e9a4cd98af54321a
darshan45672 <drshnbhandary@gmail.com>
2023-02-05 20:21:14 +0530

    code updated

--- matrix addition.c
+++ matrix addition.c
@@ -8,7 +8,7 @@ int main(void) {
 
   printf("enter the order of matrix A:\n");  // taking user input from the user
   scanf("%d%d", &m, &n);  // storing the user input in a variable
-  printf("enter the order of matrix B:\n");  //
+  printf("enter the order of matrix B:\n");  // taking user input from the user
   scanf("%d%d", &p, &q);
   if (m != p || n != q) {
     printf("\t\tSORRy!\nmatrix addition not possible!\n");
