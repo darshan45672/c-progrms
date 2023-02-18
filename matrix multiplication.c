56eb0b41353531bcbd7a4ffdd239a8378be1f42c
darshan45672 <drshnbhandary@gmail.com>
2023-02-18 12:56:32 +0530

    updated

--- matrix multiplication.c
+++ matrix multiplication.c
@@ -6,6 +6,7 @@ int main(void) {
   // start of main
 
   int a[50][50], b[50][50], c[50][50];  // declaring the required array variables
+  
   int m, n, p, q, i, j, k;
   printf("enter the order of matrix A\n");
   scanf("%d%d", &m, &n);
