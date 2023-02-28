41ff25c8aefe866a493b867a7996447f1a72f7bc
darshan45672 <drshnbhandary@gmail.com>
2023-02-28 09:18:19 +0530

    new commit

--- matrix subraction.c
+++ matrix subraction.c
@@ -32,7 +32,7 @@ int main(void) {  // start of main
     printf("enter the elements for matrix B:\n");
     for (i = 0; i < p; i++) {
       for (j = 0; j < q; j++) {
-        scanf("%d", &b[i][j]);
+        scanf("%d", &b[i][j]);  // storing the user entered elements in their 
       }
     }
     printf("matrix A is displayed as:\n");
