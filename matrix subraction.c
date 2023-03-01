614e70f847a574fba3368a8a56411d4584698feb
darshan45672 <drshnbhandary@gmail.com>
2023-03-01 07:20:04 +0530

    code updated

--- matrix subraction.c
+++ matrix subraction.c
@@ -37,6 +37,7 @@ int main(void) {  // start of main
     }
 
     /* Displaying the matrices entered by the user */
+    
     printf("matrix A is displayed as:\n");
     for (i = 0; i < m; i++) {
       for (j = 0; j < n; j++) {
