c093ea450a5e025ea8203575729a0ea5c71915e8
darshan45672 <drshnbhandary@gmail.com>
2023-03-11 18:33:34 +0530

    updated

--- no of vowels and consonants in a string.c
+++ no of vowels and consonants in a string.c
@@ -11,7 +11,7 @@ int main(void) {
   
   /* taking the input from user */
   printf("enter any string:");
-  gets(str);
+  gets(str);  // using the built in function to read the user entered st
   while(str[i]!='\0'){
   if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
     vowels++;
