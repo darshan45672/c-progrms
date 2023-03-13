e5585badb36cb430b7850fdbff1bba5578bec147
darshan45672 <drshnbhandary@gmail.com>
2023-03-13 18:30:51 +0530

    updated

--- no of vowels and consonants in a string.c
+++ no of vowels and consonants in a string.c
@@ -15,7 +15,7 @@ int main(void) {
 
   while(str[i]!='\0'){
     
-  if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){   // checking wether the character or letter 
+  if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){   // checking wether the character or letter is a vowel 
     vowels++;
   }
     else{
