81a51142ce9880e5dfb57c1345103b40bf76dd69
darshan45672 <drshnbhandary@gmail.com>
2023-03-18 19:20:44 +0530

    updated

--- no of vowels and consonants in a string.c
+++ no of vowels and consonants in a string.c
@@ -24,7 +24,7 @@ int main(void) {
     }i++;
   }
 
-  /* displaying the number of vowels and conso*/
+  /* displaying the number of vowels and consonants */
   printf("no of vowels is %d\n",vowels);
   printf("no of consonats os %d\n",consonants);
 }
