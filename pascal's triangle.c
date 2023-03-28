a1205d1c0177073c147f5905cabbf83de80a3d02
darshan45672 <drshnbhandary@gmail.com>
2023-03-29 00:29:05 +0530

    small update

--- pascal's triangle.c
+++ pascal's triangle.c
@@ -12,8 +12,11 @@ int main(void) {
   scanf("%d", &rows);  // storing the data entered by the user into respective variable
 
   for ( int i = 0; i < rows; i++) { 
+
     for (space = 1; space <= rows - i; space++) {
+
       printf(" ");
+      
     }
     for (j = 0; j <= i; j++) {
       if (j == 0 || i == 0) {
