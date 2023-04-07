0d5a09c53f5054fec4e3a6f2e7494441080fd8fd
darshan45672 <drshnbhandary@gmail.com>
2023-04-07 14:07:21 +0530

    small update

--- product of two numbers.c
+++ product of two numbers.c
@@ -5,7 +5,8 @@
 int main(void) {
 
  int a,b,res; // declaring the required varaibles
-  printf("enter the values of a and b:\n");
+
+  printf("enter the values of a and b:\n");   // taking input from the user
   scanf("%d%d",&a,&b);
   printf("product is %d",a*b);
 }
