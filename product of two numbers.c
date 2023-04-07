f3b22ee1d6db8a82e06000854b58cf0761c96eb0
darshan45672 <drshnbhandary@gmail.com>
2023-04-07 14:06:29 +0530

    small update

--- product of two numbers.c
+++ product of two numbers.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 int main(void) {
- int a,b,res;
+
+ int a,b,res; // declaring the required
   printf("enter the values of a and b:\n");
   scanf("%d%d",&a,&b);
   printf("product is %d",a*b);
