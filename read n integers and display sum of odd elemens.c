52c3dc5e03ee89491018596ee4a8acf49a713cad
darshan45672 <drshnbhandary@gmail.com>
2023-04-14 08:55:32 +0530

    small update

--- read n integers and display sum of odd elemens.c
+++ read n integers and display sum of odd elemens.c
@@ -6,13 +6,13 @@ int main(void) {
 
   int i, n, a[100], odd = 0;  // declaring the required variables
 
-  printf("enter the number of elements:\n"); // taking number of elementsas a input from the user
+  printf("enter the number of elements:\n"); // taking number of elements as a input from the user
   scanf("%d", &n);  // storing the user entered number in a variable
 
   printf("enter %d elements:\n", n); // taking the elements from the user
 
   for (i = 0; i < n; i++) {  // start of for loop
-    scanf("%d", &a[i]);
+    scanf("%d", &a[i]); // storing the user entered number in a array
   }
   for (i = 0; i < n; i++) {
     if (a[i] % 2 == 1) {
