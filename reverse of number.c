66075b13c342a74c7ad66b81b433c04f31efd38b
darshan45672 <drshnbhandary@gmail.com>
2023-04-20 08:41:53 +0530

    new update

--- reverse of number.c
+++ reverse of number.c
@@ -7,9 +7,10 @@ int main(void) { // start of main
 
   int num, temp; // declaring the required varaibles
 
-  printf("enter the number:\n"); // taking input from the user
+  printf("enter the number:\n"); // taking a number as a input from the user
   scanf("%d", &num); // storing the user entered data in a variable
-  printf("the reversed number is:\n");
+
+  printf("the reversed number is:\n"); // displaying the reversed number
   while (num != 0) {
     temp = num % 10;
     printf("%d", temp);
