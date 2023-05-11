719394d8c45458215b11a88dcbfe971670382400
darshan45672 <drshnbhandary@gmail.com>
2023-05-11 12:00:47 +0530

    new update

--- roots quadratic eqtn.c
+++ roots quadratic eqtn.c
@@ -14,7 +14,7 @@ int main(void) {  // start of main
   printf("enter the coeffient of a,b,c:\n");  // taking the ccoefficirnts of a,b,c fromt the user
   scanf("%f%f%f", &a, &b, &c); // storing the user entered data in their respective variable
 
-  /* Checking the base condition*/
+  /* Checking the initial base conditions */
   
   if (a == 0 && b == 0) {
     printf("soory!\n roots cannot be computed\n");
