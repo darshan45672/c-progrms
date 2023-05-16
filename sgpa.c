6c107c754d0e86bd8b9b9fd5f6ae210ba891999b
darshan45672 <drshnbhandary@gmail.com>
2023-05-16 19:37:57 +0530

    done

---
+++ sgpa.c
@@ -0,0 +1,15 @@
+//SGPA Calculator
+#include <stdio.h>
+
+int main(void) {
+
+  int sub1,sub2,sub3,sub4,sub5,sub6,sub7,sub8,sub9;
+  float SGPA=0;
+  printf("enter the subject grades of SEM3\n");
+  scanf("%d%d%d%d%d%d%d%d%d",&sub1,&sub2,&sub3,&sub4,&sub5,&sub6,&sub7,&sub8);
+  SGPA=(sub1*3+sub2*4+sub3*1+sub4*4+sub5*3+sub6*1+sub7*1+sub8*1)/18.0;  //this caclculates based on your grade not marks
+    
+  printf("SGPA of SEM1 is %f",SGPA);
+  
+  return 0;
+}
