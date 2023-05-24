4242cc54d0b1b8d390a44fe8f9bf4f223a3cc521
darshan45672 <drshnbhandary@gmail.com>
2023-05-24 09:32:45 +0530

    done

--- simple calculator.c
+++ simple calculator.c
@@ -8,7 +8,7 @@ int main(void) {  //start of main
   char op;
 
   /* taking input from the user */
-  printf("\nEnter a valid artematic expression:\n"); // taking a valid 
+  printf("\nEnter a valid artematic expression:\n"); // asking user to enter a valid 
   scanf("%d%c%d", &num1, &op, &num2);
   switch (op) {
   case '+':
