08060b4c660530fc5ee5b04d1f57af5879bf7d4f
darshan45672 <drshnbhandary@gmail.com>
2023-05-30 10:34:11 +0530

    done

--- simple calculator.c
+++ simple calculator.c
@@ -19,7 +19,7 @@ int main(void) {  //start of main
 
   case '-':  // if the operator is -
     res = num1 - num2; // if true then claculate the result
-    printf("\ndifference = %d", res);  // diplay the calculate result
+    printf("\ndifference = %d", res);  // diplay the calculated result
     break;
 
   case '*':
