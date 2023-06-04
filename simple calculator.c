493991e86131635bdf80337e0be6c5a164c1e443
darshan45672 <drshnbhandary@gmail.com>
2023-06-04 09:56:23 +0530

    updated

--- simple calculator.c
+++ simple calculator.c
@@ -29,7 +29,7 @@ int main(void) {  //start of main
      break;
 
    case '/': // if the operator is /
-     if (num2 == 0) {  // checking if num2is 0 
+     if (num2 == 0) {  // checking if num2 is 0 
        printf("\nreault is not defined!");
      } else
      {
