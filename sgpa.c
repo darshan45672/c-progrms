83496ad2e329aad31c06653e2454b90482469301
darshan45672 <drshnbhandary@gmail.com>
2023-07-02 09:56:14 +0530

    added comment

--- sgpa.c
+++ sgpa.c
@@ -5,8 +5,8 @@ int main(void) {  // start of main
 
   int sub1,sub2,sub3,sub4,sub5,sub6,sub7,sub8,sub9; // declare the required variables
   float SGPA=0; // declare and initialize SCPA to 0
-  
-  printf("enter the subject grades of SEM3\n");
+
+  printf("enter the subject grades of SEM3\n"); // taking the subject grades from the user
   scanf("%d%d%d%d%d%d%d%d%d",&sub1,&sub2,&sub3,&sub4,&sub5,&sub6,&sub7,&sub8);
   SGPA=(sub1*3+sub2*4+sub3*1+sub4*4+sub5*3+sub6*1+sub7*1+sub8*1)/18.0;  //this caclculates based on your grade not marks
     
