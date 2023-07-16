029ba9e60899b607a5e0d479527877bb1fc94fe9
darshan45672 <drshnbhandary@gmail.com>
2023-07-16 19:34:18 +0530

    new code updated

--- sorting.c
+++ sorting.c
@@ -5,7 +5,8 @@
 int main(void) {  // start of main
 
   int a[50], n, i, j, temp;   // declaring the required variables
-  printf("enter the number of elements\n");
+
+  printf("enter the number of elements\n");   //
   scanf("%d", &n);
   printf("enter %d elements\n",n);
   for (i = 0; i < n; i++) {
