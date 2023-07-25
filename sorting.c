898bba74ae60b3dcb726bb854c4e4cb09b5a8772
darshan45672 <drshnbhandary@gmail.com>
2023-07-25 20:42:29 +0530

    done

--- sorting.c
+++ sorting.c
@@ -16,6 +16,7 @@ int main(void) {  // start of main
 
   for (i = 1; i < n; i++) {  // starting a loop
   
+
     for (j = 0; j < n - i; i++) { // starting a 2nd loop
 
       if (a[j] > a[j + 1]) {   // comapring the 2 adjacent elements
