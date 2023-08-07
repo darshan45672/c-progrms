ebd2a2f463ac212fac76184e15eb7f8b860617df
darshan45672 <drshnbhandary@gmail.com>
2023-08-07 08:07:56 +0530

    done

--- string compare using while.c
+++ string compare using while.c
@@ -7,6 +7,7 @@ int main(void) {  // start of main
   char str1[100], str2[100];  // declaring 2 character arrays of size 100 
 
   int i, length1, length2;  // declaring the required variables
+  
   printf("enter the first string\n");
   scanf("%s", str1);
   printf("enter the second string\n");
