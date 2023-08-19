df8fe7cc2f1bc31fd582798f2f25bda946f1d4ea
darshan45672 <drshnbhandary@gmail.com>
2023-08-19 19:44:52 +0530

    updated comments

--- string concatenation.c
+++ string concatenation.c
@@ -7,7 +7,7 @@ int main(void) {  // start of main
   char s1[100],s2[100];   // declare the required variables
   
   printf("enter the first string:\t");  // taking input from the user
-  scanf("%s",s1);
+  scanf("%s",s1); // storing the user entered data in s1
   printf("\nenter the second string:\t");
   scanf("%s",s2);
   strcat(s1,s2);
