e1ac52cf2b127b8202344d13bfa4132a641e26e5
darshan45672 <drshnbhandary@gmail.com>
2023-08-21 07:59:26 +0530

    updated file

--- string_concatenation.c
+++ string_concatenation.c
@@ -1,16 +1,18 @@
 /* Program to perform string concatination using string built in functions */
 
 #include <stdio.h>
+#include<string.h>
 
 int main(void) {  // start of main
 
   char s1[100],s2[100];   // declare the required variables
   
-  printf("enter the first string:\t");  // taking input from the user
+  printf("\nenter the first string:\t");  // taking input from the user
   scanf("%s",s1); // storing the user entered data in s1
   
   printf("\nenter the second string:\t"); // taking input from the user
   scanf("%s",s2); // storing the user entered data in s1
+  
   strcat(s1,s2);
   printf("\nthe concatenated string is %s\n",s1);
 }
