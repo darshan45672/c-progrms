a842e0de7407b2f047ecde9a750bd8b8419636d5
darshan45672 <drshnbhandary@gmail.com>
2023-08-22 21:57:54 +0530

    updated file

--- string_concatenation.c
+++ string_concatenation.c
@@ -13,6 +13,6 @@ int main(void) {  // start of main
   printf("\nenter the second string:\t"); // taking input from the user
   scanf("%s",s2); // storing the user entered data in s1
   
-  strcat(s1,s2);  //
+  strcat(s1,s2);  // concatinating 2 strings using strcat()
   printf("\nthe concatenated string is %s\n",s1);
 }
