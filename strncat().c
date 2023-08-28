c42d3c1ac8f5a401d0e5994b3c9d6f7bb6a89ecf
darshan45672 <drshnbhandary@gmail.com>
2023-08-28 22:46:09 +0530

    updated

--- strncat().c
+++ strncat().c
@@ -3,20 +3,23 @@
 #include <stdio.h>  // include basic functions
 #include <string.h> // library that has string functions
 
-
 int main(void) {  // start of main
 
   char s1[20], s2[20];  // declare the required arrays
   int n;  // declaring a variable
 
   printf("enter the first string:\n");  // taking input from the user
-  gets(s1); // reading 
-  printf("enter the second string:\n");
-  gets(s2);
-  printf("enter n:\n");
-  scanf("%d", &n);
-  strncat(s1, s2, n);
+  gets(s1); // reading string 
+  printf("enter the second string:\n");   // taking 2nd input from the user
+  gets(s2); // reading string
+
+  printf("enter n:\n"); //
+  scanf("%d", &n);  //
+  strncat(s1, s2, n); //
+
   printf("the conctanated sting is\n");
   puts(s1);
-  
+
+return 0;
+
 }
