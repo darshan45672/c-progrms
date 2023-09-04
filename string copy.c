d5ee5a8e5c1c6dea73648d7101d5f541a25c9a1f
darshan45672 <drshnbhandary@gmail.com>
2023-09-04 19:19:07 +0530

    updated file

--- string copy.c
+++ string copy.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>  // including the basic header file
 #include<string.h>  // including the header file for string operattions
+
 int main(void) {
   char src[100],cpy[100];
   printf("enter the string to b copied:\t");
