dd3f16aaa49a69d324b505e9f06b1ce30ef0d671
darshan45672 <drshnbhandary@gmail.com>
2023-09-09 18:43:00 +0530

    updated file

--- string copy.c
+++ string copy.c
@@ -7,7 +7,7 @@ int main(void) {  // start of main
 
   char src[100],cpy[100]; // declare the required arrays
 
-  printf("enter the string to b copied:\t");  //
+  printf("enter the string to b copied:\t");  // taking input from the user
   scanf("%s",src);
 
   strcpy(cpy,src);
