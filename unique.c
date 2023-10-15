98d74590fb42e1d290df22a22139defc5c8a1e03
darshan45672 <drshnbhandary@gmail.com>
2023-10-15 20:34:25 +0530

    added comments for understanding

--- unique.c
+++ unique.c
@@ -1,8 +1,6 @@
 /*Implement an algorithm to determine if a string has all unique characters.*/
 /*What if you cannot use additional data structures?*/
 
-//
-
 #include<stdio.h>   //
 #include<stdlib.h>  //
 #include<string.h>  //
