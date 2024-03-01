2d9f1375e4ba1deda48b40db92abddea69861a08
darshan45672 <drshnbhandary@gmail.com>
2024-03-01 09:07:24 +0530

    added new code

--- string operations(str.h).c
+++ string operations(str.h).c
@@ -9,7 +9,7 @@ int main(void) {  // start of main
   int len1, len2; // initialise the length of the string
   int result; // declared a new variable result to store the result of the comparison
   
-  printf("enter the firt string:\n"); // taking input
+  printf("enter the firt string:\n"); // taking input from
   scanf("%s", str1);
 
   printf("enter the second string:\n");
